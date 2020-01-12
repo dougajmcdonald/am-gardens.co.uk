51cb12d56f458d824d19d5baf08c4a00921d0f57
Doug McDonald <dougajmcdonald@gmail.com>
2020-01-12 17:39:50 +0000

    few tweakz

--- src/components/hero.js
+++ src/components/hero.js
@@ -6,17 +6,16 @@ const Hero = ({ text, cta }) => (
   <section
     className="bg-cover bg-top"
     style={{
-      minHeight: `200px`,
       backgroundImage: `url("./garden-bar.png")`
     }}
   >
     <div
-      className="w-full mx-auto flex flex-wrap md:justify-between pt-6 md:pt-16 h-auto"
-      style={{ backgroundColor: `rgba(0, 0, 0, 0.375)` }}
+      className="w-full mx-auto flex flex-wrap md:justify-between py-6 md:py-16 h-auto"
+      style={{ backgroundColor: `rgba(0, 0, 0, 0.4)` }}
     >
       <div className="w-4/5 md:w-2/3 mx-auto">
         <h2
-          className="text-white font-bold text-l md:text-2xl md:mr-10 font-body mt-0 md:w-1/2 mb-6 md:mb-10"
+          className="text-white font-bold text-l md:text-2xl md:mr-10 font-body mt-0 md:w-1/2"
           style={{}}
         >
           {text}
--- src/components/quote-banner.js
+++ src/components/quote-banner.js
@@ -3,10 +3,10 @@ import React from "react"
 import Button from "../components/button"
 
 const Hero = ({ text, image }) => (
-  <div className="w-4/5 md:w-2/3 mx-auto flex md:justify-start flex-wrap my-16">
-    <div className="w-full md:w-1/3 flex">{image}</div>
+  <div className="w-4/5 md:w-2/3 mx-auto flex md:justify-start flex-wrap md:flex-no-wrap my-16">
+    <div >{image}</div>
     <div className="w-full md:w-1/2">
-      <h2 className="text-secondary font-bold text-l md:text-xl md:mr-10 font-body mt-0">
+      <h2 className="text-secondary font-semibold text-l md:text-xl md:mx-10 font-body mt-0">
         <span className="font-6xl">"</span>
         {text}
         <span className="font-6xl">"</span>
