bcab5bccbd11494a12f353c7ba86d03598c1414e
Doug McDonald <dougajmcdonald@gmail.com>
2020-01-12 13:01:21 +0000

    add full width header

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from "react"
 
 const Header = ({ siteTitle }) => (
   <footer
-    className="bg-secondary font-body flex mt-24"
+    className="bg-primary font-body flex mt-24"
     style={{ minHeight: `220px` }}
   >
     <div className="w-4/5 md:w-2/3 mx-auto text-white flex flex-column flex-wrap py-16">
@@ -11,10 +11,10 @@ const Header = ({ siteTitle }) => (
       <span className="w-full text-center">
         by <a href="https://twitter.com/dougajmcdonald">@dougajmcdonald</a>
       </span>
-      <span className="w-full text-center text-primary mt-8">
+      <span className="w-full text-center text-white mt-8">
         © {new Date().getFullYear()}, built with
         {` `}
-        <a className="text-primary" href="https://www.gatsbyjs.org">
+        <a className="text-white" href="https://www.gatsbyjs.org">
           Gatsby
         </a>
       </span>
--- src/components/hero.js
+++ src/components/hero.js
@@ -3,20 +3,26 @@ import React from "react"
 import Button from "../components/button"
 
 const Hero = ({ text, image, cta }) => (
-  <section style={{ minHeight: `200px` }}>
-    <div className="w-4/5 md:w-2/3 mx-auto flex flex-wrap md:justify-between pt-6 md:pt-16">
+  <section
+    className="bg-cover"
+    style={{ minHeight: `200px`, backgroundImage: `url("./garden-bar.png")` }}
+  >
+    <div
+      className="w-4/5 md:w-2/3 mx-auto flex flex-wrap md:justify-between pt-6 md:pt-16"
+      style={{}}
+    >
       <div className="w-full md:w-1/2">
-        <h2 className="text-secondary font-bold text-l md:text-2xl md:mr-10 font-body mt-0">
+        <h2 className="text-white font-bold text-l md:text-2xl md:mr-10 font-body mt-0">
           {text}
         </h2>
         {cta && <Button to={cta.to}>{cta.text}</Button>}
       </div>
-      <div
+      {/* <div
         className="w-full md:w-1/2 flex justify-center"
         style={{ minHeight: `200px`, minWidth: `200px` }}
       >
         {image}
-      </div>
+      </div> */}
     </div>
   </section>
 )
