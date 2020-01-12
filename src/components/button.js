6a193b4e5a3591c9fa4243bd48dd6be36714f7d6
Doug McDonald <dougajmcdonald@gmail.com>
2020-01-12 12:06:47 +0000

    update services and reduce header size

--- src/components/button.js
+++ src/components/button.js
@@ -3,10 +3,7 @@ import { Link } from "gatsby"
 
 const Button = ({ to, children }) => (
   <Link to={to}>
-    <button
-      className="bg-tertiary rounded text-white font-bold font-body text-center my-10"
-      style={{ width: `200px`, height: `60px` }}
-    >
+    <button className="bg-tertiary rounded text-white font-bold font-body text-center text-s md:text-base  my-6 md:my-10 w-32 md:w-40 h-10 md:h-16 ">
       {children}
     </button>
   </Link>
