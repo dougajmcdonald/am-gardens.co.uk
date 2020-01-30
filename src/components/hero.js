c13d32cb291a2ef4ad297648cb865cfbf4ae75f7
Doug McDonald <dougajmcdonald@gmail.com>
2020-01-30 17:37:13 +0000

    remove crappymargin

--- src/components/hero.js
+++ src/components/hero.js
@@ -5,7 +5,7 @@ const Hero = ({ text }) => (
   <section className="bg-footer md:flex inline-block justify-center">
     <div className="md:w-1/2 w-4/5 flex flex-wrap md:justify-between py-6 md:py-8 h-auto">
       <div className="w-full flex items-center hero-text">
-        <h2 className="text-white align-middle inline-block text-l md:text-xl md:mr-10 font-body mt-0 border-l-8 border-tertiary pl-4">
+        <h2 className="text-white align-middle inline-block text-l md:text-xl md:mr-10 font-body my-0 border-l-8 border-tertiary pl-4">
           {text}
         </h2>
       </div>
