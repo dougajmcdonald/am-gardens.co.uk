fd27624493db277db4f9749e3b3566ab2c8f4baf
Doug McDonald <dougajmcdonald@gmail.com>
2020-01-30 17:32:51 +0000

    tweak styling

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,15 +2,15 @@ import React from "react"
 import Image from "../components/image"
 
 const Hero = ({ text }) => (
-  <section className="bg-footer flex justify-center">
-    <div className="w-1/2 flex flex-wrap md:justify-between py-6 md:py-8 h-auto">
-      <div className="w-full flex items-center" style={{ marginLeft: `33.3%` }}>
+  <section className="bg-footer md:flex inline-block justify-center">
+    <div className="md:w-1/2 w-4/5 flex flex-wrap md:justify-between py-6 md:py-8 h-auto">
+      <div className="w-full flex items-center hero-text">
         <h2 className="text-white align-middle inline-block text-l md:text-xl md:mr-10 font-body mt-0 border-l-8 border-tertiary pl-4">
           {text}
         </h2>
       </div>
     </div>
-    <div className="bg-tertiary w-1/2 h-full float-right">
+    <div className="bg-tertiary md:w-1/2 w-full h-full hidden md:block">
       <Image />
     </div>
   </section>
--- src/components/service-list.js
+++ src/components/service-list.js
@@ -29,11 +29,11 @@ const services = [
   }
 ]
 const ServiceList = () => (
-  <div className="flex flex-row justify-between w-2/3">
+  <div className="flex flex-row justify-between w-2/3 flex-wrap">
     {services.map(serviceMap => (
-      <article className="border-tertiary border-l-8 pl-8 mt-8">
+      <article className="border-tertiary border-l-8 px-4 md:pl-8 my-4 md:my-8">
         <h3 className="mt-1 mb-4">{serviceMap.category}</h3>
-        <ul className="m-0 p-0">
+        <ul className="m-0 mb-1 p-0">
           {serviceMap.services.map(service => (
             <li className="mt-1">{service}</li>
           ))}
