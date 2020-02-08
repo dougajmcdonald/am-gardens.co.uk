46cd61552fb06ced904a6df8a9b16b5ec52906d2
Doug McDonald <dougajmcdonald@gmail.com>
2020-02-08 15:51:48 +0000

    reduce quote margins

--- src/components/quote-banner.js
+++ src/components/quote-banner.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const QuoteBanner = ({ text, image }) => (
-  <div className="w-4/5 md:w-2/3 mx-auto flex md:justify-start flex-wrap my-8 md:my-16">
+  <div className="w-4/5 md:w-2/3 mx-auto flex md:justify-start flex-wrap my-4 md:my-8">
     <div className="w-full md:w-2/3">{image}</div>
     <div className="w-full md:w-2/3 mt-4">
       <h2 className="text-secondary font-normal mt-0 italic text-l md:text-l lg:text-xl font-body border-tertiary border-l-8 pl-4">
