79aca7cc9f9cace5eafa3f871bc61ce2c6d7948d
Doug McDonald <dougajmcdonald@gmail.com>
2020-02-08 17:34:40 +0000

    yarn and fix cta wrapping

--- src/components/cta-banner.js
+++ src/components/cta-banner.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const CTABanner = ({ children }) => (
   <div className="bg-tertiary w-full text-white py-2">
-    <span className="cta-text">{children}</span>
+    <span className="cta-text inline-block">{children}</span>
   </div>
 )
 
